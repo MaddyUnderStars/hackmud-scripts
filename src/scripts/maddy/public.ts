8de8b71ab4000bf0217ff8bd83f95cd7e09fb6a4
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2025-01-24 15:38:44 +1100

    feat(public): create public script

---
+++ src/scripts/maddy/public.ts
@@ -0,0 +1,42 @@
+import { table } from "/lib/table";
+
+export default (context: Context) => {
+    const header = `                  :                                                                       
+       %#%@*%=%@%=*++-*%%  %@. %*  #%+                                                    
+      %:%% %%% .*@%%@%=. -@- %%   %=        +%                                            
+     .%%%+%@ #%%=--   *%%*  @- =%=   %% #@.    -  #*                    ..                
+     %%+@%.#%@ +%@%%%@%+  %#  #+.*@*+*- .    -      .  %                #%#               
+    %%@%-%%#+*%%@%@=                              %%+      %            +%.@              
+    %%@@@%#                                              -%-  *         *% @%        @@%  
+                                                               % #      %## %@   .%@%-%   
+                                                                       -% % :@@%# %- %    
+                                                                       %% -=.#: @. %%     
+    ███╗   ███╗ █████╗ ██████╗ ██████╗ ██╗   ██╗    .#%%% % == %   . # @      
+    ████╗ ████║██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝         % +%.% @ - @ =  *    .. *%       
+    ██╔████╔██║███████║██║  ██║██║  ██║ ╚████╔╝             *%% % @  - .  .  * =  :=      
+    ██║╚██╔╝██║██╔══██║██║  ██║██║  ██║  ╚██╔╝                   %@  #  *  : :     :*     
+    ██║ ╚═╝ ██║██║  ██║██████╔╝██████╔╝   ██║                        @@             -@    
+    ╚═╝     ╚═╝╚═╝  ╚═╝╚═════╝ ╚═════╝    ╚═╝                        @                %   
+                  understars                                         #    %   %%.      =  
+                                                                    %   %          @#     
+                                                                   - -%                 @ 
+                                                                   .                      
+`;
+
+    // biome-ignore lint/style/useTemplate: <explanation>
+    return `${header}\n` +
+        "All scripts listed here are `2free of charge` and do not contain any malicious payloads.\n" +
+        "`DThe same cannot be said for scripts not listed here.`\n\n" +
+        `${table([
+            ["SEC", "script", "desc"],
+            [],
+            ["`2FS`", "maddy.read", "Decorrupt script output"],
+            ["`2FS`", "maddy.t1", "T1 cracker"],
+            ["`2FS`", "maddy.search_t1", "Really bad t1 corp loc search"],
+            [],
+            ["`HMS`", "maddy.scripts", "Public scripts viewer. MS due to chats join/leave"],
+            [],
+            ["`FLS`", "maddy.log", "sys.access_log viewer. Shows all unread logs made by users"],
+        ], context.cols)
+        }`;
+};
