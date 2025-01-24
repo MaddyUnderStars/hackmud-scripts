17b05d7b1f123299d8e1043dfc65936b829763ac
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2025-01-24 15:38:34 +1100

    feat(read): make public

--- src/scripts/maddy/read.ts
+++ src/scripts/maddy/read.ts
@@ -3,69 +3,49 @@
 import { isRecord } from "/lib/isRecord";
 import { isScriptor } from "/lib/isScriptor";
 
-function findLastIndex<T>(array: Array<T>, predicate: (value: T, index: number, obj: T[]) => boolean): number {
-    let l = array.length;
-    while (l--) {
-        if (predicate(array[l], l, array))
-            return l;
-    }
-    return -1;
-}
-
 export default function (context: Context, args?: unknown) {
-    if (!isRecord(args)) return { ok: false };
-    if (!isScriptor(args.s)) return { ok: false };
+    if (!isRecord(args) || !isScriptor(args.s))
+        return { ok: false, msg: "maddy.read { s: #s.trust.public_corp, a: { passthrough }, timings?: true }" };
 
     const passthrough = args.a;
 
     const chars = $fs.scripts.lib().corruption_chars;
-    const colourChars = $fs.scripts.lib().colors;
 
     let ret = "";
+    const timings = [];
     do {
+        const start = Date.now();
         const corruptedOutput = args.s.call(passthrough);
         let iter: string;
         if (typeof corruptedOutput === "string") iter = corruptedOutput;
-        else if (isRecord(corruptedOutput) &&
-            'msg' in corruptedOutput &&
-            typeof corruptedOutput.msg === "string")
+        else if (
+            isRecord(corruptedOutput) &&
+            "msg" in corruptedOutput &&
+            typeof corruptedOutput.msg === "string"
+        )
             iter = corruptedOutput.msg;
         else if (Array.isArray(corruptedOutput)) iter = corruptedOutput.join("\n");
         else return { ok: false, msg: "failed" };
 
-        iter = iter.replaceAll(/\`[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ](.*?)\`/g, (s, ...args) => {
-            return args[0];
-        })
+        iter = iter.replaceAll(
+            /\`[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ](.*?)\`/g,
+            (s, ...args) => {
+                return args[0];
+            },
+        );
 
         if (ret === "") ret = iter;
 
-        // ret = ret.split("").map((char, i, arr) => {
-        //     if (!chars.includes(char)) return char;
+        ret = ret
+            .split("")
+            .map((char, i) => (!chars.includes(char) ? char : iter[i]))
+            .join("");
 
-        //     arr[i - 1] = iter[i - 1];
-        //     arr[i + 1] = iter[i + 1];
-        //     return iter[i];
-        // }).join("");
+        timings.push(Date.now() - start);
+    } while (chars.split("").find((x) => ret.includes(x)));
 
-        ret = ret.split("").map((char, i) => !chars.includes(char) ? char : iter[i]).join("");
-
-        // const searchtext = ret.substring(-10);
-        // const startpos = iter.indexOf(searchtext.substr(findLastIndex(searchtext.split(""), x => chars.includes(x))));
-
-        // if (startpos === -1) continue;
-
-        // const longestReadable = iter.substring(startpos + 1).split("").findIndex(x => chars.includes(x));
-        // if (!longestReadable) continue;
-
-        // const line = iter.substring(ret.length, longestReadable);
-        // // $D(`${JSON.stringify({
-        // //     searchtext: searchtext.replaceAll('"', '\"'),
-        // //     startpos,
-        // //     line: line.replaceAll('"', '\"'),
-        // //     ret: ret.replaceAll('"', '\"'),
-        // // }, null, 2)}\n`);
-        // ret += line;
-    } while (chars.split("").find(x => ret.includes(x)));
+    if (args.timings)
+        return `${ret}\n\n${timings.reduce((prev, curr) => prev + curr) / (timings.length - 1)}ms`;
 
     return ret;
 }
