318899a743a9f68277cd76707989f1f57d616048
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2025-01-27 18:21:37 +1100

    feat(xfer): render amount as gc str

--- src/scripts/squizzy/xfer.ts
+++ src/scripts/squizzy/xfer.ts
@@ -2,6 +2,8 @@ import { ALLOWED_USERS } from "/lib/auth";
 import { isRecord } from "/lib/isRecord";
 
 export default function (context: Context, args?: unknown) {
+	const l = $fs.scripts.lib();
+
 	if (
 		isRecord(args) &&
 		"amount" in args &&
@@ -10,13 +12,12 @@ export default function (context: Context, args?: unknown) {
 		(typeof args.amount === "string" || typeof args.amount === "number")
 	) {
 		$fs.accts.xfer_gc_to_caller({ amount: args.amount });
-		return args.amount;
+		return l.to_gc_str($fs.accts.balance_of_owner());
 	}
 
 	$ms.accts.xfer_gc_to({ to: "squizzy", amount: $hs.accts.balance() });
 
 	if (ALLOWED_USERS.includes(context.caller)) {
-		const l = $fs.scripts.lib();
 		return l.to_gc_str($fs.accts.balance_of_owner());
 	}
 }
