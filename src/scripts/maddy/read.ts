fa4db8099b76d882f3d3182a61df7b8349f38c00
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2025-01-27 18:19:16 +1100

    feat(read): allow perm corruption through

--- src/scripts/maddy/read.ts
+++ src/scripts/maddy/read.ts
@@ -4,48 +4,72 @@ import { isRecord } from "/lib/isRecord";
 import { isScriptor } from "/lib/isScriptor";
 
 export default function (context: Context, args?: unknown) {
-    if (!isRecord(args) || !isScriptor(args.s))
-        return { ok: false, msg: "maddy.read { s: #s.trust.public_corp, a: { passthrough }, timings?: true }" };
+	if (!isRecord(args) || !isScriptor(args.s))
+		return {
+			ok: false,
+			msg: "maddy.read { s: #s.trust.public_corp, a: { passthrough }, timings?: true }",
+		};
 
-    const passthrough = args.a;
+	const passthrough = args.a;
 
-    const chars = $fs.scripts.lib().corruption_chars;
+	const chars = $fs.scripts.lib().corruption_chars;
 
-    let ret = "";
-    const timings = [];
-    do {
-        const start = Date.now();
-        const corruptedOutput = args.s.call(passthrough);
-        let iter: string;
-        if (typeof corruptedOutput === "string") iter = corruptedOutput;
-        else if (
-            isRecord(corruptedOutput) &&
-            "msg" in corruptedOutput &&
-            typeof corruptedOutput.msg === "string"
-        )
-            iter = corruptedOutput.msg;
-        else if (Array.isArray(corruptedOutput)) iter = corruptedOutput.join("\n");
-        else return { ok: false, msg: "failed" };
+	const ignored: number[] = [];
 
-        iter = iter.replaceAll(
-            /\`[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ](.*?)\`/g,
-            (s, ...args) => {
-                return args[0];
-            },
-        );
+	let attempts = 0;
+	let ret = "";
+	const timings = [];
+	do {
+		if (_END - Date.now() < 500) break;
 
-        if (ret === "") ret = iter;
+		const start = Date.now();
+		const corruptedOutput = args.s.call(passthrough);
+		let iter: string;
+		if (typeof corruptedOutput === "string") iter = corruptedOutput;
+		else if (
+			isRecord(corruptedOutput) &&
+			"msg" in corruptedOutput &&
+			typeof corruptedOutput.msg === "string"
+		)
+			iter = corruptedOutput.msg;
+		else if (Array.isArray(corruptedOutput)) iter = corruptedOutput.join("\n");
+		else return { ok: false, msg: "failed" };
 
-        ret = ret
-            .split("")
-            .map((char, i) => (!chars.includes(char) ? char : iter[i]))
-            .join("");
+		iter = iter.replaceAll(
+			/\`[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ](.*?)\`/g,
+			(s, ...args) => {
+				if (chars.includes(args[0])) return args[0];
 
-        timings.push(Date.now() - start);
-    } while (chars.split("").find((x) => ret.includes(x)));
+				return s;
+			},
+		);
 
-    if (args.timings)
-        return `${ret}\n\n${timings.reduce((prev, curr) => prev + curr) / (timings.length - 1)}ms`;
+		if (ret === "") ret = iter;
 
-    return ret;
+		ret = ret
+			.split("")
+			.map((char, i) => {
+				if (!chars.includes(char) || ignored.includes(i)) return char;
+
+				if (attempts > 0 && char === iter[i]) {
+					ignored.push(i);
+				}
+
+				return iter[i];
+			})
+			.join("");
+
+		attempts++;
+
+		timings.push(Date.now() - start);
+	} while (
+		chars
+			.split("")
+			.find((x) => ret.includes(x) && !ignored.includes(ret.indexOf(x)))
+	);
+
+	if (args.timings)
+		return `${ret}\n\navg ${timings.length === 1 ? timings[0] : timings.reduce((prev, curr) => prev + curr) / (timings.length - 1)}ms. calls: ${attempts}. ignored_indexes: ${JSON.stringify(ignored)}`;
+
+	return ret;
 }
