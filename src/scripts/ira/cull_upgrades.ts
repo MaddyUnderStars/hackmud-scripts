f455b633739c30633be68cfd28b95657d19d8ed0
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2025-01-31 15:50:04 +1100

    feat: sell upgrades instead of culling

--- src/scripts/ira/cull_upgrades.ts
+++ src/scripts/ira/cull_upgrades.ts
@@ -1,19 +1,90 @@
+import { isRecord } from "/lib/isRecord";
+import { table } from "/lib/table";
+
 export default function (context: Context, args?: unknown) {
 	const trash = $hs.sys.upgrades({
 		filter: {
 			loaded: false,
 		},
+		full: true,
 	});
 
 	if ("ok" in trash && !trash.ok) return trash;
 
 	if (!Array.isArray(trash)) return trash;
 
-	$ls.sys.cull({
-		i: trash
-			.filter((x) => x.rarity < 2)
-			.filter((x) => !x.name.includes("k3y"))
-			.map((x) => x.i),
-		confirm: true,
-	});
+	const filtered = trash
+		.filter((x) => x.rarity < 3)
+		.filter((x) => !x.loaded)
+		.filter((x) => !x.name.includes("k3y"));
+
+	const lib = $fs.scripts.lib();
+
+	const log = [];
+
+	for (const item of filtered) {
+		const s = JSON.parse(JSON.stringify(item));
+		for (const key of ["i", "sn", "loaded", "description"]) delete s[key];
+
+		const market = $ls.market.browse(s);
+
+		if (!Array.isArray(market) || !market.length) continue;
+
+		// const sellCost =
+		// market.reduce((prev, curr) => prev + curr.cost, 0) / (market.length - 1);
+
+		// get the new index of this up
+		const index = (
+			$hs.sys.upgrades({ full: true }) as { sn: string; i: number }[]
+		).find((x) => x.sn === item.sn);
+
+		if (!index) continue;
+
+		const sellCost = market[0].cost;
+
+		const listFee = Math.max(sellCost * 0.05, 1_000);
+
+		if (listFee > sellCost && isRecord(args) && "dry" in args && !args.dry) {
+			$ls.sys.cull({
+				i: index.i,
+				confirm: true,
+			});
+			continue;
+		}
+
+		if (!sellCost) continue;
+
+		if (isRecord(args) && "dry" in args && !args.dry) {
+			const bal = $hs.accts.balance();
+			if (bal < listFee) $ms.squizzy.xfer({ amount: Math.ceil(listFee) });
+
+			const ret = $ls.market.sell({
+				i: index.i,
+				cost: Math.floor(sellCost),
+				confirm: true,
+			});
+
+			if (!ret.ok) return ret;
+		}
+
+		log.push([
+			`${index.i}`,
+			`\`${item.rarity}${item.name}\``,
+			lib.to_gc_str(sellCost),
+			lib.to_gc_str(listFee),
+		]);
+	}
+
+	return table(
+		[["i", "name", "sell price", "list fee"], [], ...log],
+		context.cols,
+	);
+
+	// $ls.sys.cull({
+	// 	i: trash
+	// 		.filter((x) => x.rarity < 2)
+	// 		.filter((x) => !x.name.includes("k3y"))
+	// 		.map((x) => x.i),
+	// 	confirm: true,
+	// });
 }
