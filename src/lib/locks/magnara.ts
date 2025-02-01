8bbb6531c46a3baf9e96f2227b940bdff88fb8a1
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2025-02-01 15:50:24 +1100

    feat(magnara): only write to db once

--- src/lib/locks/magnara.ts
+++ src/lib/locks/magnara.ts
@@ -1,38 +1,49 @@
 import { permute } from "../permute";
 import type { LockSolver } from "./type";
 
-export const magnara: LockSolver = function* () {
-    const input = yield { magnara: "" };
-    const word: string = input.split(" ").splice(-1)[0];
+export const magnara: LockSolver = function* (context, log) {
+	const input = yield { magnara: "" };
+	const word: string = input.split(" ").splice(-1)[0];
 
-    const sorted = word
-        .split("")
-        .sort((a, b) => a.localeCompare(b))
-        .join("");
-    const existing = $db
-        .f({ _id: `magnara_${sorted}`, word: { $type: "string" } })
-        .first();
-    if (existing) {
-        yield { magnara: existing.word };
+	log(`word is ${word}`);
 
-        // if the existing word didn't work, remove it
-        $db.r({ _id: `magnara_${sorted}` });
-    }
+	const sorted = word
+		.split("")
+		.sort((a, b) => a.localeCompare(b))
+		.join("");
+	const existing = $db
+		.f({ _id: `magnara_${sorted}`, word: { $type: "string" } })
+		.first();
+	if (existing) {
+		log(`magnara db had ${existing.word}`);
+		yield { magnara: existing.word };
 
-    const LIMIT = 6;
+		log("bad word");
 
-    // if the word is small enough, just brute it
-    if (word.length < LIMIT) {
-        const gen = permute(word.split(""));
-        for (const perm of gen) {
-            const word = perm.join("");
+		// if the existing word didn't work, remove it
+		$db.r({ _id: `magnara_${sorted}` });
+	}
 
-            // I'd rather only do 1 db opt,
-            // but that would involve being able to signal to a solver when it's done
-            // which would be messy at the moment.
-            $db.us({ _id: `magnara_${sorted}` }, { $set: { word } });
+	log("no magnara dict, bruting");
 
-            yield { magnara: word };
-        }
-    }
+	const LIMIT = 6;
+
+	// if the word is small enough, just brute it
+	if (word.length < LIMIT) {
+		const gen = permute(word.split(""));
+		let found: string | undefined;
+		try {
+			for (const perm of gen) {
+				found = perm.join("");
+				yield { magnara: found };
+			}
+		} finally {
+			if (!found) {
+				log("no word found");
+			} else {
+				log(`found word ${word}`);
+				$db.us({ _id: `magnara_${sorted}` }, { $set: { found } });
+			}
+		}
+	}
 };
