17da8d796d1f54421bd23bcdbe249d6596c06426
Madeline <46743919+MaddyUnderStars@users.noreply.github.com>
2025-02-08 14:10:48 +1100

    refactor(cull_upgrades): don't request fee from bank multiple times + more

--- src/scripts/ira/cull_upgrades.ts
+++ src/scripts/ira/cull_upgrades.ts
@@ -1,3 +1,4 @@
+import { throwFailure } from "/lib/failure";
 import { isRecord } from "/lib/isRecord";
 import { table } from "/lib/table";
 
@@ -13,121 +14,130 @@ export default function (context: Context, args?: unknown) {
 
 	if (!Array.isArray(trash)) return trash;
 
+	const lib = $fs.scripts.lib();
+
 	const filtered = trash
 		.filter((x) => x.rarity <= 3)
 		.filter((x) => !x.loaded)
 		.filter((x) => !x.name.includes("k3y"));
 
-	const lib = $fs.scripts.lib();
-
-	const log = [];
+	const operations: {
+		op: "sell" | "cull" | "store";
+		msg?: string;
+		sn: string;
+		name: string;
+		cost?: number;
+		fee?: number;
+		ret?: number;
+	}[] = [];
 
 	for (const item of filtered) {
+		if (item.rarity > 2) {
+			operations.push({
+				op: "store",
+				sn: item.sn,
+				name: `\`${item.rarity}${item.name}\``,
+				msg: "rarity > 2",
+			});
+            continue;
+		}
+
 		const s = JSON.parse(JSON.stringify(item));
 		for (const key of ["i", "sn", "loaded", "description"]) delete s[key];
 
-        if (s.cooldown) s.cooldown = { $gte: s.cooldown }
-        if (s.amount) s.amount = { $gte: s.amount }
-        if (s.cost) s.cost = { $gte: s.cost }
-        if (s.count) s.count = { $lte: s.count }
-        if (s.chars) s.chars = { $lte: s.chars }
-        if (s.max_glock_amnt) s.max_glock_amnt = { $lte: s.max_glock_amnt }
-        if (s.expire_secs) s.expire_secs = { $gte: s.expire_secs }
+		if (s.cooldown) s.cooldown = { $gte: s.cooldown };
+		if (s.amount) s.amount = { $gte: s.amount };
+		if (s.cost) s.cost = { $gte: s.cost };
+		if (s.count) s.count = { $lte: s.count };
+		if (s.chars) s.chars = { $lte: s.chars };
+		if (s.max_glock_amnt) s.max_glock_amnt = { $lte: s.max_glock_amnt };
+		if (s.expire_secs) s.expire_secs = { $gte: s.expire_secs };
 
 		const market = $ls.market.browse(s);
 
-		const index = (
-			$hs.sys.upgrades({ full: true }) as {
-				sn: string;
-				i: number;
-				rarity: number;
-			}[]
-		).find((x) => x.sn === item.sn);
-
-		if (!index) continue;
-
 		if (!Array.isArray(market) || !market.length) {
-			if (index.rarity < 3) {
-				if (isRecord(args) && "dry" in args && !args.dry)
-					$ls.sys.cull({
-						i: index.i,
-						confirm: true,
-					});
-
-				log.push([
-					`${index.i}`,
-					`\`${item.rarity}${item.name}\``,
-					"",
-					"",
-					"CULL no market",
-				]);
-			}
-
+			operations.push({
+				op: "cull",
+				name: `\`${item.rarity}${item.name}\``,
+				msg: "no market",
+				sn: item.sn,
+			});
 			continue;
 		}
-
-		// const sellCost =
-		// market.reduce((prev, curr) => prev + curr.cost, 0) / (market.length - 1);
-
-		// get the new index of this up
 
 		const sellCost = market[0].cost;
 		const returnValue = sellCost * 0.9;
-
 		const listFee = Math.max(sellCost * 0.05, 1_000);
 
 		if (listFee >= returnValue) {
-			log.push([
-				`${index.i}`,
-				`\`${item.rarity}${item.name}\``,
-				"",
-				"",
-				"CULL poor return",
-			]);
-
-			if (isRecord(args) && "dry" in args && !args.dry)
-				$ls.sys.cull({
-					i: index.i,
-					confirm: true,
-				});
-
+			operations.push({
+				op: "cull",
+				msg: "poor return",
+				name: `\`${item.rarity}${item.name}\``,
+				sn: item.sn,
+				cost: sellCost,
+			});
 			continue;
 		}
 
-		if (!sellCost) continue;
+		operations.push({
+			op: "sell",
+			sn: item.sn,
+			name: `\`${item.rarity}${item.name}\``,
+			cost: sellCost,
+			fee: listFee,
+			ret: returnValue,
+		});
+	}
 
-		if (isRecord(args) && "dry" in args && !args.dry) {
-			const bal = $hs.accts.balance();
-			if (bal < listFee) $ms.maddy.xfer({ amount: Math.ceil(listFee) });
+	if (isRecord(args) && "dry" in args && !args.dry) {
+		const totalFees = operations
+			.map((x) => x.fee ?? 0)
+			.reduce((prev, curr) => prev + curr, 0);
 
-			const ret = $ls.market.sell({
-				i: index.i,
-				cost: Math.floor(sellCost),
-				confirm: true,
-			});
+		$ms.maddy.xfer({ amount: Math.ceil(totalFees) });
 
-			if (!ret.ok) return ret;
+		for (const item of operations) {
+			const index = throwFailure($hs.sys.upgrades({ full: true })).find(
+				(x) => x.sn === item.sn,
+			)?.i;
+			if (!index) continue;
+
+			if (item.op === "cull") {
+				throwFailure(
+					$ls.sys.cull({
+						i: index,
+						confirm: true,
+					}),
+				);
+				continue;
+			}
+
+            if (item.op === "store") continue;
+
+			if (!item.cost) continue;
+
+			throwFailure(
+				$ls.market.sell({
+					i: index,
+					cost: Math.floor(item.cost),
+					confirm: true,
+				}),
+			);
 		}
-
-		log.push([
-			`${index.i}`,
-			`\`${item.rarity}${item.name}\``,
-			lib.to_gc_str(sellCost),
-			lib.to_gc_str(listFee),
-			"sell",
-		]);
 	}
 
 	return table(
-		[["i", "name", "sell price", "list fee", "action"], [], ...log],
+		[
+			["name", "return value", "action", "msg"],
+			[],
+			...operations.map((x) => [
+				x.name,
+				x.ret ? lib.to_gc_str(x.ret) : "",
+				x.op,
+				x.msg ?? "",
+			]),
+		],
 		context.cols,
 	);
-
-	// $ls.sys.cull({
-	// 	i: trash
-	// 		.filter((x) => x.rarity < 2)
-	// 		.filter((x) => !x.name.includes("k3y"))
-	// 		.map((x) => x.i),
-	// 	confirm: true,
-	// });
 }
